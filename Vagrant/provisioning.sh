5519f9ba0766c0d3fff27e4bcfd5d1146fe970d3
U-MNSUK\b8886963 <b8886963@LH008127.mnsuk.adroot.marksandspencer.com>
2015-03-07 13:29:20 +0000

    Steve: altered folder structure

---
+++ Vagrant/provisioning.sh
@@ -0,0 +1,18 @@
+yum install -y java-1.7.0-openjdk.x86_64
+yum install -y mlocate
+yum install -y telnet
+yum install -y screen
+yum install -y vim
+yum install -y git
+
+
+git clone -b pull18  https://github.com/stevengonsalvez/kibana-authentication-proxy.git
+git clone https://github.com/stevengonsalvez/vagrant-e1.4k3.git
+
+sed -i -e 's|#adminpass=|adminpass='$password'|' ./vagrant-e1.4k3/install_vagrant-e1.4k3_90_centos.sh
+sed -i -e 's|echo -n "Enter a password to use for the admin account to login to the vagrant-e1.4k3 webUI: "|#echo -n "Enter a password to use for the admin account to login to the vagrant-e1.4k3 webUI: "|' ./vagrant-e1.4k3/install_vagrant-e1.4k3_90_centos.sh
+sed -i -e 's|read adminpass|#read adminpass|' ./vagrant-e1.4k3/install_vagrant-e1.4k3_90_centos.sh
+sed -i -e 's|pause 'Press [Enter] key to continue...'|#pause 'Press [Enter] key to continue...'|' ./vagrant-e1.4k3/install_vagrant-e1.4k3_90_centos.sh
+chmod +x ./vagrant-e1.4k3/install_vagrant-e1.4k3_90_centos.sh
+./vagrant-e1.4k3/install_vagrant-e1.4k3_90_centos.sh
+rm -rf ./vagrant-e1.4k3
--- provisioining/preinstall.sh
+++
@@ -1,9 +0,0 @@
-yum install -y java-1.7.0-openjdk.x86_64
-yum install -y mlocate
-yum install -y telnet
-yum install -y screen
-yum install -y vim
-yum install -y git
-
-
-git clone -b pull18 --single-branch https://github.com/stevengonsalvez/kibana-authentication-proxy.git
