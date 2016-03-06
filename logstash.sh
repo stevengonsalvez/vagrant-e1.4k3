9d92829dfa68af8c7960b07f6d3edbbee72d59c5
steven gonsalvez <steven.gonsalvez@gmail.com>
2016-03-06 22:37:25 +0000

    steve: added latest

--- logstash.sh
+++ logstash.sh
@@ -10,3 +10,10 @@ gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
 enabled=1
 EOL
 '
+sudo yum install -y logstash
+sudo  chkconfig --add logstash
+sudo /opt/logstash/bin/plugin install logstash-input-jdbc
+sudo wget -P /tmp http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.38.tar.gz
+sudo mkdir -p /opt/logstash/drivers
+sudo tar -zxvf /tmp/mysql-connector-java*.tar.gz -C /opt/logstash/drivers
+sudo chown -R logstash:logstash /opt/logstash/drivers
